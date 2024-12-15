040f7d394d661e7f12b20be5626926685e5b569a
11307703 <11307703@student.pxl.be>
2024-12-15 20:02:48 +0100

    finaal

---
+++ 9Getallen/9Getallen/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _9Getallen
+{
+    internal class Program
+    {
+        private static int[] numbers = new int[9];
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getal opvragen uit 9");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < numbers.Length;i++)
+            {
+                Console.Write($"Geef getal {i+1}/9 : ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            
+            Console.Write("Welk getal van de 9 getallen wens je opniew te zien? ");
+            int index = int.Parse(Console.ReadLine());
+            Console.Write($"Jouw getal is {numbers[index-1]}");
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
